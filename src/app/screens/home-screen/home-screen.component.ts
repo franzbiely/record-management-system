01e8e4ea058b08d2c96b3ba2962fa883aad7fa95
Gina Carpen <mrthemetribe@gmail.com>
2018-11-16 06:47:46 +0800

    - updated home, list view and support page

--- src/app/screens/home-screen/home-screen.component.ts
+++ src/app/screens/home-screen/home-screen.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeScreenComponent implements OnInit {
 
+  households = Household;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+export interface Households{
+  name: string;
+  value: number;
+  date: string;
+}
+
+export interface Proposals{
+  name: string;
+  subname: string;
+  value: number;
+  action: string;
+}
+
+export interface Documents{
+  imgsrc: string;
+  name: string;
+  subname: string;
+  date: string;
+  action: string;
+}
+
+const Household:Households[] = [
+  {name: "Candelaria Household", value: 917000, date: "7/31/2018"},
+  {name: "Fernen Household", value: 300000, date: "7/31/2018"},
+  {name: "Mackrill Household", value: 2000000, date: "7/31/2018"},
+  {name: "Atkinson Family", value: 500000, date: "7/31/2018"},
+  {name: "Matthews Foundation", value: 50000, date: "7/31/2018"},
+];
+
+// const Proposal:Proposals[] = [
+//   {},
+//   {},
+//   {},
+//   {},
+//   {},
+//   {},
+// ];
