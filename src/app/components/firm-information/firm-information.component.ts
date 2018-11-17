ab2abedc784657f45c3529096cb4e7a5f74f5c33
Gina Carpen <mrthemetribe@gmail.com>
2018-11-17 11:46:40 +0800

    updated added

--- src/app/components/firm-information/firm-information.component.ts
+++ src/app/components/firm-information/firm-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-firm-information',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirmInformationComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    $(document).ready(function(){
+      var nr =0;
+      $('#droppable').on({
+        'dragover dragenter': function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+        },
+        'drop': function(e, ui) {
+          //console.log(e.originalEvent instanceof DragEvent);
+          var dataTransfer = e.originalEvent.dataTransfer;
+          if (dataTransfer && dataTransfer.files.length) {
+            e.preventDefault();
+            e.stopPropagation();
+            $.each(dataTransfer.files, function(i, file) {
+              var reader = new FileReader();
+              reader.onload = $.proxy(function(file, $fileList, event) {
+                nr++;
+                var img = file.type.match('image.*') ? "<img  id=\"resizable"+nr+"\" class=\"resizable\" src='" + event.target.result + "' /> " : "";
+                $fileList.prepend($("<span>").append(img + file.name));
+                // this.setResizable("resizable"+nr);
+              }, this, file, $("#fileList"));
+              reader.readAsDataURL(file);
+            });
+          }
+          $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+        }
 
-  ngOnInit() {
+        
+      });
+    });  
+
   }
 
+  setResizable(id){
+    // $("#" + id).resizable({
+    //   stop: function(event, ui) {
+    //     height = $("#" + id).height();
+    //     width = $("#" + id).width();
+    //     console.log("width=height=" + width + "==" + height);
+    //   }
+    // });
+  }
+
+  ngOnInit() {
+    
+  }
+
+  
+
+ 
 }
