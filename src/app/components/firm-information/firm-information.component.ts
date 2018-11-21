2319c1573265511ba4eff38e315bda6a646ad66e
Francis Albores <mrthemetribe@gmail.com>
2018-11-21 22:31:47 +0800

    Refactor Firm Information

--- src/app/components/firm-information/firm-information.component.ts
+++ src/app/components/firm-information/firm-information.component.ts
@@ -8,37 +8,35 @@ import * as $ from 'jquery';
 })
 export class FirmInformationComponent implements OnInit {
 
+  step:number = 1;
+
   constructor() { 
     $(document).ready(function(){
       var nr =0;
-      $('#browse-holder').on({
-        'dragover dragenter': function(e) {
+      $(document).on('dragover dragenter', '#browse-holder', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+      }).on( 'drop', '#browse-holder', function(e, ui) {
+        var dataTransfer = e.originalEvent.dataTransfer;
+        if (dataTransfer && dataTransfer.files.length) {
           e.preventDefault();
           e.stopPropagation();
-        },
-        'drop': function(e, ui) {
-          var dataTransfer = e.originalEvent.dataTransfer;
-          if (dataTransfer && dataTransfer.files.length) {
-            e.preventDefault();
-            e.stopPropagation();
-            $.each(dataTransfer.files, function(i, file) {
-              var reader = new FileReader();
-              reader.onload = $.proxy(function(file, $fileList, event) {
-                nr++;
-                var img = file.type.match('image.*') ? "<img  id=\"resizable"+nr+"\" class=\"resizable\" style='width: 200px;'  src='" + event.target.result + "' /> " : "";
-                $fileList.append(img);
+          $.each(dataTransfer.files, function(i, file) {
+            var reader = new FileReader();
+            reader.onload = $.proxy(function(file, $fileList, event) {
+              nr++;
+              var img = file.type.match('image.*') ? "<img  id=\"resizable"+nr+"\" class=\"resizable\" style='width: 200px;'  src='" + event.target.result + "' /> " : "";
+              $fileList.append(img);
 
-              }, this, file, $("#droppable"));
-              $("#browse-holder").css("justify-content", "space-between");
-              $("#droppable").css("display", "flex");
-              reader.readAsDataURL(file);
-            });
-          }
-          $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+            }, this, file, $("#droppable"));
+            $("#browse-holder").css("justify-content", "space-between");
+            $("#droppable").css("display", "flex");
+            reader.readAsDataURL(file);
+          });
         }
-      });
-    });  
-
+        $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+      })
+    });
   }
 
   setResizable(id) {
@@ -48,8 +46,13 @@ export class FirmInformationComponent implements OnInit {
   ngOnInit() {
     
   }
-
-  
+  toPrev() {
+    this.step--;
+  }
+  toNext() {
+    // do some transactions here...
+    this.step++;
+  }
 
  
 }
