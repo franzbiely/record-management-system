d56feaec0cb9d540c9ec03798d31f00b213b2dc8
Francis Albores <mrthemetribe@gmail.com>
2018-11-27 22:29:41 +0800

    Link back to create household

--- src/app/components/proposal/proposal.component.ts
+++ src/app/components/proposal/proposal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-proposal',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProposalComponent implements OnInit {
 
+	toHousehold() {
+		this.router.navigateByUrl('create-household');
+	}
 
-  constructor() { }
+  constructor(private router: Router) { }
 
 
   ngOnInit() {
