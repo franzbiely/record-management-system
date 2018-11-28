0f655ed481231beebee53ad9665fefb7befb1f8a
Francis Albores <mrthemetribe@gmail.com>
2018-11-28 14:55:42 +0800

    Step Mover

--- src/app/components/risk-assessment/risk-assessment.component.ts
+++ src/app/components/risk-assessment/risk-assessment.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RiskAssessmentComponent implements OnInit {
 	step:number = 1;
+	moveTo($step) {
+		this.step = $step;
+	}
   constructor() { }
 
   ngOnInit() {
