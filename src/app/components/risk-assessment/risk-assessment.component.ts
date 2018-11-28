24ea6c697cb7e079ec5ab000059a52d0f3edc47c
Francis Albores <mrthemetribe@gmail.com>
2018-11-28 14:39:08 +0800

    Set First link for Risk

--- src/app/components/risk-assessment/risk-assessment.component.ts
+++ src/app/components/risk-assessment/risk-assessment.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risk-assessment.component.scss']
 })
 export class RiskAssessmentComponent implements OnInit {
-
+	step:number = 1;
   constructor() { }
 
   ngOnInit() {
