0486f5599a16500e6a1986afb203b051b2f5ccc6
Francis Albores <mrthemetribe@gmail.com>
2018-12-06 00:34:27 +0800

    simulation responsive fixes

--- src/app/components/simulation/simulation.component.ts
+++ src/app/components/simulation/simulation.component.ts
@@ -14,6 +14,8 @@ export class SimulationComponent implements OnInit {
   @ViewChild('recommended_portfolio_chart_large', { read: ElementRef }) private recommended_portfolio_chart_large_ref: ElementRef;
   @ViewChild('piea_chart', { read: ElementRef }) private piea_chart_ref: ElementRef;
   @ViewChild('pieb_chart', { read: ElementRef }) private pieb_chart_ref: ElementRef;
+  @ViewChild('piea_chart_mobile', { read: ElementRef }) private piea_chart_mobile_ref: ElementRef;
+  @ViewChild('pieb_chart_mobile', { read: ElementRef }) private pieb_chart_mobile_ref: ElementRef;
   
 
 
@@ -25,6 +27,8 @@ export class SimulationComponent implements OnInit {
   recommended_portfolio_chart_large: any;
   piea_chart: any;
   pieb_chart: any;
+  piea_chart_mobile: any;
+  pieb_chart_mobile: any;
 
 
   constructor(private data: DataService) { }
@@ -36,37 +40,55 @@ export class SimulationComponent implements OnInit {
   }
 
   ngAfterViewInit() {
+    const piea_chart_data = {
+      labels: ['2020', '2025', '2030'],
+      datasets: [{
+          fill: true,
+          data: [80, 15, 5],
+          borderWidth: 0,
+          backgroundColor : ['#465E7B', '#3D98CB', '#404041']
+      }]
+    }
+    const piea_chart_options = {
+      legend: { display: false },
+      plugins: {
+          strokeShadow: {},
+          datalabels: { display: false }
+      }
+    }
+    const responsive_options = { responsive : true, maintainAspectRatio: false }
+    const pieb_chart_data = {
+      labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
+      datasets: [{
+          fill: true,
+          data: [45, 5, 5, 5, 50, 10, 10, 5, 10],
+          borderWidth: 0,
+          backgroundColor : ['#42834F','#F6BC80','#AED68F','#B95780','#F7C98B', '#ED7963', '#A9D7DF', '#465E7B', '#67C077' ]
+      }]
+    }
     this.piea_chart = new Chart(this.piea_chart_ref.nativeElement, {
       type: 'doughnut',
-      data: {
-        labels: ['2020', '2025', '2030'],
-        datasets: [{
-            fill: true,
-            data: [80, 15, 5],
-            borderWidth: 0,
-            backgroundColor : ['#465E7B', '#3D98CB', '#404041']
-        }]
-      },
-      options: {
-        legend: { display: false }
-      }
-    }, { responsive : true, maintainAspectRatio: false });
-    
+      data: piea_chart_data,
+      options: piea_chart_options
+    }, responsive_options);
+    this.piea_chart_mobile = new Chart(this.piea_chart_mobile_ref.nativeElement, {
+      type: 'doughnut',
+      data: piea_chart_data,
+      options: piea_chart_options
+    }, responsive_options);
+
     this.pieb_chart = new Chart(this.pieb_chart_ref.nativeElement, {
       type: 'doughnut',
-      data: {
-        labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
-        datasets: [{
-            fill: true,
-            data: [45, 5, 5, 5, 50, 10, 10, 5, 10],
-            borderWidth: 0,
-            backgroundColor : ['#42834F','#F6BC80','#AED68F','#B95780','#F7C98B', '#ED7963', '#A9D7DF', '#465E7B', '#67C077' ]
-        }]
-      },
-      options: {
-        legend: { display: false }
-      }
-    }, { responsive : true, maintainAspectRatio: false });
+      data: pieb_chart_data,
+      options: piea_chart_options
+    }, responsive_options);
+    this.pieb_chart_mobile = new Chart(this.pieb_chart_mobile_ref.nativeElement, {
+      type: 'doughnut',
+      data: pieb_chart_data,
+      options: piea_chart_options
+    }, responsive_options);
+
+
     
     this.recommended_portfolio_chart = new Chart(this.recommended_portfolio_chart_ref.nativeElement, {
       type: 'line',
@@ -88,6 +110,12 @@ export class SimulationComponent implements OnInit {
       },
       options: {
         legend: { display: true, position: 'bottom' },
+        plugins: {
+            strokeShadow: {},
+            datalabels: {
+                display: false
+            }
+        },
         scales: {
             yAxes: [{
                 ticks: {
@@ -129,6 +157,12 @@ export class SimulationComponent implements OnInit {
       },
       options: {
         legend: { display: true, position: 'bottom' },
+        plugins: {
+            strokeShadow: {},
+            datalabels: {
+                display: false
+            }
+        },
         scales: {
             yAxes: [{
                 ticks: {
