45b5f4d5283200a18c97576aa2eda67b984d9f69
Francis Albores <mrthemetribe@gmail.com>
2018-12-09 20:24:49 +0800

    Import account modal > import button

--- src/app/components/import-accounts/import-accounts.component.ts
+++ src/app/components/import-accounts/import-accounts.component.ts
@@ -7,6 +7,7 @@ import { ModalStatusService } from "../../services/modal-status.service"
   styleUrls: ['./import-accounts.component.scss']
 })
 export class ImportAccountsComponent implements OnInit {
+  selected_counter: number = 0;
 	importAccountsModal: boolean = false;
   accountDetailsModal: boolean = false;
   constructor(private modalStatus: ModalStatusService) { }
@@ -22,6 +23,10 @@ export class ImportAccountsComponent implements OnInit {
 
   closeMe() {
   	this.modalStatus.toggleImportAccount(false);
+  }
+  selectAccount(event) {
+    this.selected_counter = (event.checked) ? this.selected_counter+1 : this.selected_counter-1;
+    console.log(this.selected_counter);
   }
 
 }
