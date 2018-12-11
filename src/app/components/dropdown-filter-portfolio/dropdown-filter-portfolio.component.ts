541670fafa409417c6edb443eff77487f44274a0
Francis Albores <mrthemetribe@gmail.com>
2018-12-11 23:54:15 +0800

    Portfolio Administration > Filter

--- src/app/components/dropdown-filter-portfolio/dropdown-filter-portfolio.component.ts
+++ src/app/components/dropdown-filter-portfolio/dropdown-filter-portfolio.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DropdownFilterPortfolioComponent implements OnInit {
 
+  filter_data = {
+  	riskname_and_range : {
+  		0 : true, //all
+  		1 : true,
+  		2 : true,
+  		3 : true,
+  		4 : true,
+  		5 : true,
+  		6 : true,
+  		7 : true,
+  	},
+  	portfolios : {
+  		0 : true, //all
+  		1 : true,
+  		2 : true,
+  		3 : true,
+  	}
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectAllToggler(event, type) {
+  	if(event.checked) {
+  		Object.keys(this.filter_data[type]).forEach(v => this.filter_data[type][v] = true)	
+  		console.log(this.filter_data)
+  	}
+  	else {
+  		Object.keys(this.filter_data[type]).forEach(v => this.filter_data[type][v] = false)
+  		console.log(this.filter_data)
+  	}
   }
 
 }
