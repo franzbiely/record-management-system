7f52199f959e676e6993ae7dd24d44c93c2c5eaa
Francis Albores <mrthemetribe@gmail.com>
2018-12-15 21:58:53 +0800

    Portfolio > Use provided checkbox icons in dropdown filter. If any selected, button become blue

--- src/app/components/dropdown-filter/dropdown-filter.component.ts
+++ src/app/components/dropdown-filter/dropdown-filter.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown-filter.component.scss']
 })
 export class DropdownFilterComponent implements OnInit {
-
+	checkCounter: number = 0;
+	checkAll : boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checkFilter(event, isForAll) {
+  	if(event.checked) {
+  		if(isForAll) {
+  			this.checkAll = true;
+  			this.checkCounter = 4;
+  		}
+  		else {
+  			this.checkCounter++;	
+  		}
+  	}
+  	else {
+  		if(isForAll) {
+  			this.checkAll = false;
+  			this.checkCounter = 0;
+  		}
+  		else {
+  			this.checkCounter--;	
+  		}
+  	}
   }
 
 }
