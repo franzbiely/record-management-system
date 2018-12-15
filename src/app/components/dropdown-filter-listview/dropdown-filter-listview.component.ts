84ab6ea647ee0598a436f6500c94194da5005fc9
Francis Albores <mrthemetribe@gmail.com>
2018-12-15 23:44:07 +0800

    Filter fix for list view

--- src/app/components/dropdown-filter-listview/dropdown-filter-listview.component.ts
+++ src/app/components/dropdown-filter-listview/dropdown-filter-listview.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown-filter-listview.component.scss']
 })
 export class DropdownFilterListviewComponent implements OnInit {
+	checkCounter: number = 0;
+	checkAll : boolean = false;
+  
+	filter_data = {
+  	household : {
+  		0 : false, //all
+  		1 : false,
+  		2 : false,
+  	},
+  	proposal : {
+  		0 : false, //all
+  		1 : false,
+  		2 : false,
+  		3 : false,
+  		4 : false,
+  	}
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectAllToggler(event, type) {
+  	if(event.checked) {
+  		Object.keys(this.filter_data[type]).forEach(v => this.filter_data[type][v] = true)	
+  		console.log(this.filter_data)
+  		this.checkAll = true;
+  		this.checkCounter = 4;
+  	}
+  	else {
+  		Object.keys(this.filter_data[type]).forEach(v => this.filter_data[type][v] = false)
+  		console.log(this.filter_data)
+  		this.checkAll = false;
+  		this.checkCounter = 0;
+  	}
+  }
+
+  checkFilter(event, isForAll) {
+  	if(event.checked) {
+  		this.checkCounter++;
+  	}
+  	else {
+  		this.checkCounter--;
+  	}
   }
 
 }
