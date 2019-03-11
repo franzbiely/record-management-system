a75d3fa15f83e43e4ae0326427ffb5efbc66c2bb
themetribe <mrthemetribe@gmail.com>
2019-03-12 01:24:37 +0800

    account details update with infinite scroll

--- src/app/components/account-details/account-details.component.ts
+++ src/app/components/account-details/account-details.component.ts
@@ -3,6 +3,8 @@ import { ModalStatusService } from "../../services/modal-status.service";
 import { DataService } from "../../services/data.service";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+const nisPackage = require('../../../../package.json');
+
 @Component({
   selector: 'app-account-details',
   templateUrl: './account-details.component.html',
@@ -16,6 +18,8 @@ export class AccountDetailsComponent implements OnInit {
 	accountDetailsModal: boolean = false;
   enableAccountInfo : boolean = false;
   showSuggestion: boolean = false;
+  haveAllocationData: boolean = false;
+
   frmAI = {
     f1 : '',
     f2 : 'Manually',
@@ -23,7 +27,18 @@ export class AccountDetailsComponent implements OnInit {
     f6 : 'IRA (Roth)',
     f7 : 'Chris Candelaria'
   }
-  constructor(private modalStatus: ModalStatusService, private data: DataService, private fb: FormBuilder) { }
+  array = [];
+  sum = 100;
+  throttle = 300;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
+  direction = '';
+  
+  nisVersion = nisPackage.dependencies['ngx-infinite-scroll'];
+
+  constructor(private modalStatus: ModalStatusService, private data: DataService, private fb: FormBuilder) {
+    this.appendItems(0, this.sum);
+   }
 
   ngOnInit() {
     this.modalStatus.importAccount.subscribe(value => this.importAccountsModal = value);
@@ -41,6 +56,46 @@ export class AccountDetailsComponent implements OnInit {
     })
   }
 
+  addItems(startIndex, endIndex, _method) {
+    for (let i = 0; i < this.sum; ++i) {
+      this.array[_method]({
+        'name': 'International Business Machines Cor',
+        'id': 'IBM',
+        'asset': 'Cash & Cash Alternatives',
+        'quantity': '155.75',
+        'allocation': '11%',
+        'value': '$20,000.00'
+      });
+    }
+  }
+  appendItems(startIndex, endIndex) {
+    this.addItems(startIndex, endIndex, 'push');
+  }
+  
+  prependItems(startIndex, endIndex) {
+    this.addItems(startIndex, endIndex, 'unshift');
+  }
+
+  onScrollDown (ev) {
+    console.log('scrolled down!!', ev);
+
+    // add another 20 items
+    const start = this.sum;
+    this.sum += 20;
+    this.appendItems(start, this.sum);
+    
+    this.direction = 'down'
+  }
+  
+  onUp(ev) {
+    console.log('scrolled up!', ev);
+    const start = this.sum;
+    this.sum += 20;
+    this.prependItems(start, this.sum);
+  
+    this.direction = 'up';
+  }
+
   closeMe() {
   	this.modalStatus.toggleAccountDetails(false);
   }
@@ -53,6 +108,7 @@ export class AccountDetailsComponent implements OnInit {
     this.data.SET_accountDetailsType(type);
   }
   clickSuggestion() {
+    this.haveAllocationData = true;
     this.showSuggestion = false;
   }
   onSearchChange(searchValue : string) {
