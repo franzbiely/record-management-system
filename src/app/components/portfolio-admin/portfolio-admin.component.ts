94959ca3177b0df33fd92046508ffa4fc497e327
themetribe <mrthemetribe@gmail.com>
2019-03-12 20:34:36 +0800

    - portfolio administration sortable datable fixed

--- src/app/components/portfolio-admin/portfolio-admin.component.ts
+++ src/app/components/portfolio-admin/portfolio-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalStatusService } from "../../services/modal-status.service"
-import { DataService } from "../../services/data.service"
+import { ModalStatusService } from "../../services/modal-status.service";
+import { DataService } from "../../services/data.service";
+import { OrderPipe } from 'ngx-order-pipe';
 
 @Component({
   selector: 'app-portfolio-admin',
@@ -11,7 +12,134 @@ export class PortfolioAdminComponent implements OnInit {
   viewPortfolioModal: boolean = false;
   portfolioPopType : string|'view'|'edit' = 'view';
 
-  constructor(private modalStatus: ModalStatusService, private data: DataService) { }
+
+  order: string = 'name';
+  reverse: boolean = false;
+  sortedCollection: any[];
+
+  array = [
+    {
+      'name': 'Example Advisors - Balanced Select 1',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 2',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 3',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 4',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 5',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 6',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 7',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 8',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 9',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 10',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 11',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 12',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    },
+    {
+      'name': 'Example Advisors - Balanced Select 13',
+      'score': '29',
+      'risk': {
+        'name' :'Balanced',
+        'value' : '30-43'
+      },
+      'group' : 'Example Advisors Portfolios'
+    }
+  ]
+
+  constructor(private modalStatus: ModalStatusService, private data: DataService, private orderPipe: OrderPipe) {
+    this.sortedCollection = orderPipe.transform(this.array, 'name');
+   }
 
   ngOnInit() {
     this.modalStatus.viewPortfolio.subscribe(value => this.viewPortfolioModal = value);
@@ -27,6 +155,13 @@ export class PortfolioAdminComponent implements OnInit {
     this.data.SET_portfolioPopupType('edit');
     this.modalStatus.SET_viewPortfolio(true);
     window.scroll(0,0);
+  }
+  setOrder(value: string) {
+    if (this.order === value) {
+      this.reverse = !this.reverse;
+    }
+    console.log(value)
+    this.order = value;
   }
 
 }
